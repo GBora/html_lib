f5b7ae03055f3cb57a99a6d7dd558af06e43ca05
GBora <gpbora@gmail.com>
2023-11-26 23:07:03 +0200

    more tags

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export const li = tagFunctionFactory('li');
 export const div = tagFunctionFactory('div');
 export const ol = tagFunctionFactory('ol');
 export const ul = tagFunctionFactory('ul');
+export const img = tagFunctionFactory('img');
+export const form = tagFunctionFactory('form');
